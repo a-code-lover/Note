53cc24b18bd81ceee56bbde45e7ce7a91a787dcf
AndrewKe <mrandrewke@gmail.com>
2019-03-14 10:42:35 +0800

    update0314

--- c++/c++_implementation.cpp
+++ c++/c++_implementation.cpp
@@ -148,6 +148,35 @@ void quicksort(ForwardIt first, ForwardIt last)
     quicksort(middle2, last);
 }
 
+// template<class InputIt1, class InputIt2, class OutputIt>
+// OutputIt merge(InputIt1 first1, InputIt1 last1,
+//                InputIt2 first2, InputIt2 last2,
+//                OutputIt d_first)
+// {
+//     for (; first1 != last1; ++d_first) {
+//         if (first2 == last2) {
+//             return std::copy(first1, last1, d_first);
+//         }
+//         if (*first2 < *first1) {
+//             *d_first = *first2;
+//             ++first2;
+//         } else 
+//             *d_first = *first1;
+//             ++first1;
+//         }
+//     }
+//     return std::copy(first2, last2, d_first);
+// }
+
+// template <class ForwardIter, class OutputIter
+// void mergeSort(ForwardIter first, ForwardIter last, OutputIter outFirst) {
+//     if (first == last) return;
+//     auto mid = std::next(first, std::distance(first, last)/2);
+//     mergeSort(first, mid);
+//     mergeSort(mid+1, last);
+//     merge();
+// }
+
 // Summary: next_permutaion for array without or without duplication.
 // Parameters:
 // Return:
